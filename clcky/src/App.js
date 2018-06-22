43ff84d2407eb0d3b142bdde47b82bb4d6e9f00a
brianclauzel <brianthomasclauzel@gmail.com>
2018-06-21 22:20:05 -0700

    score and shuffling working

--- clcky/src/App.js
+++ clcky/src/App.js
@@ -1,20 +1,72 @@
 import React, { Component } from 'react';
 import ImageCard from "./components/ImageCard";
 import Wrapper from "./components/Wrapper";
-import images from "./images.json";
+import defaultImages from "./images.json";
 import './App.css';
 import PointCounter from './components/PointCounter';
 
 class App extends Component {
 
   state = {
-    images,
-    score: "0",
-    topScore: "0"
+    images: defaultImages,
+    score: 0,
+    topScore: 0,
+    clickedOnArray: []
   };
 
-  randomize = image => {
-    const images = this.state.images[Math.floor(image * Math.random())];
+  shuffle = () => {
+    var images = this.state.images;
+    var m = images.length, t, i;
+    while (m) {
+      i = Math.floor(Math.random() * m--);
+      t = images[m];
+      images[m] = images[i];
+      images[i] = t;
+    }
+    this.setState({images: images});
+  }
+
+  reset = () => {
+    const {images} = this.state;
+    // loop over images and set isClicked to false
+    images.forEach(image => {
+      image.clickedOn = false;
+    });
+    this.setState({images: images, score: 0, });
+  }
+
+  correct = (id) => {
+    var {images, score, topScore} = this.state;
+    if (score > 11) {
+      alert("You beat the game! good job, fool.");
+      this.reset();
+    }
+    else {
+      images[id].clickedOn = true
+      this.setState({images: images, score: score+1});
+    }
+  }
+
+  incorrect = (id) => {
+    console.log("you lost, please play again");
+      alert("You clicked the same one twice!");
+      this.reset();
+  }
+
+
+  counter = (id) => {
+    var {images, score, topScore} = this.state;
+    if (images[id].clickedOn) {
+      this.incorrect(id);
+    }
+    else {
+      this.correct(id);
+    }
+    this.shuffle();
+  }
+
+  componentDidMount = () => {
+    this.shuffle();
   }
 
   render() {
@@ -22,16 +74,16 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Clicky Brew Game</h1>
-          <PointCounter />
+          <PointCounter score={this.state.score} topScore={this.state.topScore}/>
         </header>
         <p className="App-intro">
           Click as many images as you can to gain points, but you can only click each one once.
         </p>
         <Wrapper>
-        {this.state.images.map(image => (
+        {this.state.images.map((image, i) =>   (
           <ImageCard
-            randomize={this.randomize}
-            id={image.id}
+            counter={this.counter}
+            id={i}
             key={image.id}
             name={image.name}
             image={image.image}
--- clcky/src/components/ImageCard/ImageCard.js
+++ clcky/src/components/ImageCard/ImageCard.js
@@ -4,7 +4,7 @@ import "./ImageCard.css";
 const ImageCard = props => (
     <div className="card">
         <div className="img-container">
-            <img className="image" onClick={() => props.randomize(props.image)} alt={props.name} src={props.image} />
+            <img className="image" onClick={() => props.counter(props.id)} alt={props.name} src={props.image} />
         </div>
     </div>
 )
